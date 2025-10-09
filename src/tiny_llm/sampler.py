19494a528d4c1a08ca7d5b329e426b9528fda8c3
jasmine <2939361916@qq.com>
2025-10-09 21:41:47 +0800

    week_1 day_7 task_1

--- src/tiny_llm/sampler.py
+++ src/tiny_llm/sampler.py
@@ -6,6 +6,21 @@ def make_sampler(temp: float, top_p: float, top_k: int | None):
     def sample(logprobs: mx.array):
         if temp == 0:
             return mx.argmax(logprobs, axis=-1)
-        pass
+        # 1. top-k
+        if top_k is not None and top_k > 0:
+            mask_elements = mx.argpartition(-logprobs, kth=top_k - 1, axis=-1)[:, top_k:]
+            logprobs[:, mask_elements] = -mx.inf
+        # 2. top-p
+        if top_p is not None and top_p > 0:
+            sorted_idx = mx.argsort(-logprobs, axis=-1)
+            sorted_logprobs = logprobs[:, sorted_idx]
+            cumsum = mx.cumsum(mx.exp(sorted_logprobs), axis=-1)    # cumulative probs
+            mask_elements = cumsum < top_p
+            mask_elements[..., 0] = True  # always keep the first one
+            logprobs[:, sorted_idx] = mx.where(mask_elements, sorted_logprobs, -mx.inf)
+        # 3. temperature scaling
+        logprobs = logprobs / temp
+        # 4. sample
+        return mx.random.categorical(logprobs, axis=-1)
 
     return sample
