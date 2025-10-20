51d62a73602cca999a4cf90da643951713e842b3
Mathew Teoh <mteoh@linkedin.com>
2025-10-20 00:33:46 -0700

    actually pass all week1day2task1
    needed to add in offset

--- src/tiny_llm/positional_encoding.py
+++ src/tiny_llm/positional_encoding.py
@@ -21,8 +21,6 @@ class RoPE:
         ).reshape(1, -1)
         # freqs shape is (seq_len, 1, dims / 2)
         freqs = mx.matmul(p_vec, theta_vec).reshape(seq_len, 1, -1)
-        # freqs shape is (seq_len, dims / 2)
-        # freqs = mx.matmul(p_vec, theta_vec)
 
         # shape of each of cos and sin freqs are (seq_len, 1, dims / 2)
         self.cos_freqs = mx.cos(freqs)
@@ -31,24 +29,24 @@ class RoPE:
     def __call__(
         self, x: mx.array, offset: list[slice] | slice | None = None
     ) -> mx.array:
-        # x is shape (N, L, H, D). we want it to be (N, H, L, D // 2, 2)
+        # x is shape (N, L, H, D).
         nlh_dims = x.shape[:-1]
         seq_len = min(nlh_dims[1], self.seq_len)
         # (N, L, H, D) -> (N, L, H, D//2, 2)
         x = x.reshape(*nlh_dims, self.dims // 2, 2)
-        # (N, L, H, D//2, 2) -> (N, H, L, D//2, 2)
-        # x = x.swapaxes(-3, -4)
 
-        # shape: (N, H, L, D//2)
-        out_left = mx.multiply(x[..., 0], self.cos_freqs[:seq_len, :]) - mx.multiply(
-            x[..., 1], self.sin_freqs[:seq_len, :]
-        )
-        # shape: (N, H, L, D//2)
-        out_right = mx.multiply(x[..., 0], self.sin_freqs[:seq_len, :]) + mx.multiply(
-            x[..., 1], self.cos_freqs[:seq_len, :]
-        )
-        # shape: (N, H, L, D//2, 2) -> (N, L, H, D//2, 2)
-        # out = mx.stack([out_left, out_right], axis=-1).swapaxes(-3, -4)
+        if offset:
+            cos_freqs = self.cos_freqs[offset, :]
+            sin_freqs = self.sin_freqs[offset, :]
+        else:
+            cos_freqs = self.cos_freqs[:seq_len, :]
+            sin_freqs = self.sin_freqs[:seq_len, :]
+
+        x_left, x_right = x[..., 0], x[..., 1]
+        # shape: (N, L, H, D//2)
+        out_left = mx.multiply(x_left, cos_freqs) - mx.multiply(x_right, sin_freqs)
+        # shape: (N, L, H, D//2)
+        out_right = mx.multiply(x_left, sin_freqs) + mx.multiply(x_right, cos_freqs)
         # shape: (N, L, H, D//2, 2)
         out = mx.stack([out_left, out_right], axis=-1)
         # shape: (N, L, H, D)
