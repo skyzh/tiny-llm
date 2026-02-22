aa071b97ce81a723dc1e137cda96940eb92c2d35
Connor1996 <zbk602423539@gmail.com>
2026-02-22 13:45:00 -0800

    tests: parametrize day4 flash attention mask coverage

--- tests_refsol/test_week_2_day_4.py
+++ tests_refsol/test_week_2_day_4.py
@@ -4,56 +4,67 @@ from .tiny_llm_base import *
 from .utils import *
 
 
-def attention_helper(stream: mx.Stream, H_q, H, L, E, S, BATCH):
+def attention_helper(stream: mx.Stream, H_q, H, L, E, S, BATCH, with_mask: bool):
     precision = mx.float32
     with mx.stream(stream):
         q_shape = (BATCH, H_q, L, E)
         kv_shape = (BATCH, H, S, E)
+        mask_shape = (BATCH, H_q, L, S)
         scale = 0.9
         for _ in range(100):
             query = mx.random.uniform(shape=q_shape, dtype=precision)
             key = mx.random.uniform(shape=kv_shape, dtype=precision)
             value = mx.random.uniform(shape=kv_shape, dtype=precision)
+            mask = mx.random.uniform(shape=mask_shape, dtype=precision) if with_mask else None
 
             reference_output = mx.fast.scaled_dot_product_attention(
                 q=query,
                 k=key,
                 v=value,
                 scale=scale,
+                mask=mask,
             )
             user_output = flash_attention(
                 query,
                 key,
                 value,
                 scale=scale,
+                mask=mask,
             )
             mx.eval(user_output)  # so that any error will be caught here
             assert_allclose(user_output, reference_output, precision=mx.float16)
 
 
-def test_flash_attention_cpu_small():
-    attention_helper(mx.cpu, 6, 3, 2, 5, 3, 1)
+@pytest.mark.parametrize("with_mask", [False, True], ids=["no_mask", "mask"])
+def test_task_2_flash_attention_cpu_small(with_mask: bool):
+    attention_helper(mx.cpu, 6, 3, 2, 5, 3, 1, with_mask)
 
 
-def test_flash_attention_cpu():
-    attention_helper(mx.cpu, 18, 6, 7, 5, 3, 10)
+@pytest.mark.parametrize("with_mask", [False, True], ids=["no_mask", "mask"])
+def test_task_2_flash_attention_cpu(with_mask: bool):
+    attention_helper(mx.cpu, 18, 6, 7, 5, 3, 10, with_mask)
 
 
-def test_flash_attention_cpu_large():
-    attention_helper(mx.cpu, 28, 4, 16, 128, 16, 3)
+@pytest.mark.parametrize("with_mask", [False, True], ids=["no_mask", "mask"])
+def test_task_2_flash_attention_cpu_large(with_mask: bool):
+    attention_helper(mx.cpu, 28, 4, 16, 128, 16, 3, with_mask)
 
 
-def test_flash_attention_gpu_extra_small():
-    attention_helper(mx.gpu, 1, 1, 5, 7, 4, 1)
+@pytest.mark.parametrize("with_mask", [False, True], ids=["no_mask", "mask"])
+def test_task_3_flash_attention_gpu_extra_small(with_mask: bool):
+    attention_helper(mx.gpu, 1, 1, 5, 7, 4, 1, with_mask)
 
 
-def test_flash_attention_gpu_small():
-    attention_helper(mx.gpu, 6, 3, 2, 5, 3, 1)
+@pytest.mark.parametrize("with_mask", [False, True], ids=["no_mask", "mask"])
+def test_task_3_flash_attention_gpu_small(with_mask: bool):
+    attention_helper(mx.gpu, 6, 3, 2, 5, 3, 1, with_mask)
 
 
-def test_flash_attention_gpu():
-    attention_helper(mx.gpu, 18, 6, 7, 5, 3, 10)
+@pytest.mark.parametrize("with_mask", [False, True], ids=["no_mask", "mask"])
+def test_task_3_flash_attention_gpu(with_mask: bool):
+    attention_helper(mx.gpu, 18, 6, 7, 5, 3, 10, with_mask)
 
 
-def test_flash_attention_gpu_large():
-    attention_helper(mx.gpu, 28, 4, 16, 128, 16, 3)
+@pytest.mark.parametrize("with_mask", [False, True], ids=["no_mask", "mask"])
+def test_task_3_flash_attention_gpu_large(with_mask: bool):
+    attention_helper(mx.gpu, 28, 4, 16, 128, 16, 3, with_mask)
